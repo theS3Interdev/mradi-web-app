bbbbeb5d92e77073b9778faea6f994e143b393af
Michael Owuor <michael.owuor@gmail.com>
2022-09-22 02:51:00 +0300

    feat: create signup page

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -1,5 +1,60 @@
+import { useState } from 'react';
+
 const Signup = () => {
-	return <div>Signup Page</div>;
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [displayName, setDisplayName] = useState('');
+	const [thumbnail, setThumbnail] = useState(null);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(email, password, displayName);
+	};
+
+	return (
+		<form onSubmit={handleSubmit} className="auth-form">
+			<h2 className="font-semibold uppercase">Sign Up</h2>
+			<label>
+				<span>Email:</span>
+				<input
+					type="email"
+					onChange={(e) => setEmail(e.target.value)}
+					value={email}
+					required
+					className="rounded-lg"
+				/>
+			</label>
+
+			<label>
+				<span>Password:</span>
+				<input
+					type="password"
+					onChange={(e) => setPassword(e.target.value)}
+					value={password}
+					required
+					className="rounded-lg"
+				/>
+			</label>
+
+			<label>
+				<span>Display Name:</span>
+				<input
+					type="text"
+					onChange={(e) => setDisplayName(e.target.value)}
+					value={displayName}
+					required
+					className="rounded-lg"
+				/>
+			</label>
+
+			<label>
+				<span>Profile Thumbnail:</span>
+				<input required type="file" />
+			</label>
+
+			<button className="btn">Sign Up</button>
+		</form>
+	);
 };
 
 export default Signup;
