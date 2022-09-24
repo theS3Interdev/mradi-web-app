af1f09a816439f9069c1f83e57e688e82292df2f
Michael Owuor <michael.owuor@gmail.com>
2022-09-24 16:58:31 +0300

    feat: conditional navbar links (#15)

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
+import { useAuthContext } from '../hooks/use-auth-context';
 import { useSignout } from '../hooks/use-signout';
 import MradiLogo from '../assets/mradi.svg';
 
 const Navbar = () => {
 	const { signout, isPending } = useSignout();
+	const { user } = useAuthContext();
 
 	return (
 		<nav className="navbar">
@@ -13,25 +15,32 @@ const Navbar = () => {
 					<Link to="/">Mradi by S3</Link>
 				</li>
 
-				<li>
-					<Link to="/signin">Sign In</Link>
-				</li>
-				<li>
-					<Link to="/signup">Sign Up</Link>
-				</li>
-				<li>
-					{!isPending && (
-						<button className="btn" onClick={signout}>
-							Sign Out
-						</button>
-					)}
+				{!user && (
+					<>
+						<li>
+							<Link to="/signin">Sign In</Link>
+						</li>
+						<li>
+							<Link to="/signup">Sign Up</Link>
+						</li>
+					</>
+				)}
 
-					{isPending && (
-						<button className="btn" disabled>
-							Signing Out...
-						</button>
-					)}
-				</li>
+				{user && (
+					<li>
+						{!isPending && (
+							<button className="btn" onClick={signout}>
+								Sign Out
+							</button>
+						)}
+
+						{isPending && (
+							<button className="btn" disabled>
+								Signing Out...
+							</button>
+						)}
+					</li>
+				)}
 			</ul>
 		</nav>
 	);
