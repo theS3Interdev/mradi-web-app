eeb6eb2248ef9fea4b7ca5b2aa5d9e3e73989d30
Michael Owuor <michael.owuor@gmail.com>
2022-09-24 16:02:10 +0300

    feat: create a sign in page (#12)

--- src/pages/signin/signin.jsx
+++ src/pages/signin/signin.jsx
@@ -1,5 +1,42 @@
+import { useState } from 'react';
+
 const Signin = () => {
-	return <div>Signin Page</div>;
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(email, password);
+	};
+
+	return (
+		<form onSubmit={handleSubmit} className="auth-form">
+			<h2 className="font-semibold uppercase">Sign In</h2>
+			<label>
+				<span>Email:</span>
+				<input
+					type="email"
+					onChange={(e) => setEmail(e.target.value)}
+					value={email}
+					required
+					className="rounded"
+				/>
+			</label>
+
+			<label>
+				<span>Password:</span>
+				<input
+					type="password"
+					onChange={(e) => setPassword(e.target.value)}
+					value={password}
+					required
+					className="rounded"
+				/>
+			</label>
+
+			<button className="btn">Sign In</button>
+		</form>
+	);
 };
 
 export default Signin;
