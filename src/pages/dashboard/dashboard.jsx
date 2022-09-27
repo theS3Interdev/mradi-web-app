8505ac713c1227fade68dcf49ac26468caa3cb75
Michael Owuor <michael.owuor@gmail.com>
2022-09-27 13:33:55 +0300

    making a filter - part 1

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,5 +1,6 @@
 import { useCollection } from '../../hooks/use-collection';
 import ProjectList from '../../components/project-list';
+import ProjectFilter from './project-filter';
 
 const Dashboard = () => {
 	const { documents, error } = useCollection('projects');
@@ -9,6 +10,8 @@ const Dashboard = () => {
 			<h2 className="page-title">Dashboard</h2>
 
 			{error && <p className="error">{error}</p>}
+
+			{documents && <ProjectFilter />}
 
 			{documents && <ProjectList projects={documents} />}
 		</div>
---
+++ src/pages/dashboard/project-filter.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+const filterList = ['all', 'mine', 'development', 'design', 'marketing', 'sales'];
+
+const ProjectFilter = () => {
+	const [currentFilter, setCurrentFilter] = useState('all');
+
+	const handleClick = (newFilter) => {
+		setCurrentFilter(newFilter);
+	};
+
+	return (
+		<div className="project-filter">
+			<nav>
+				<p>Filter by: </p>
+				{filterList.map((f) => (
+					<button
+						key={f}
+						onClick={() => handleClick(f)}
+						className={currentFilter === f ? 'active' : ''}
+					>
+						{f}
+					</button>
+				))}
+			</nav>
+		</div>
+	);
+};
+
+export default ProjectFilter;
